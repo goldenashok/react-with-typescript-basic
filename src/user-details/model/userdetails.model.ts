f901b8addd7e7d90950773d9dcd670ac261a7d12
Ashok Kumar D <goldenashok@users.noreply.github.com>
2020-07-30 19:12:21 +0530

    Delete userdetails.model.ts

--- src/user-details/model/userdetails.model.ts
+++
@@ -1,9 +0,0 @@
-export interface userDetails {
-    show: boolean,
-    userPeriods: Array<userDetailPeriods>
-}
-
-export interface userDetailPeriods {
-    start_time: string,
-    end_time: string
-}
